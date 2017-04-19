d0ef9b927c7126bae3a365459fc4d16c05db7631
BbekShr <bbeksh@Bibeks-MacBook-Pro.local>
2017-04-19 18:32:34 -0500

    abc
    abc

---
+++ main.cpp
@@ -0,0 +1,18 @@
+
+
+
+#include <iostream>
+#include "sha512.h"
+
+using std::string;
+using std::cout;
+using std::endl;
+
+int main(int argc, char *argv[])
+{
+    string input = "abc";
+    string output1 = sha512(input);
+    
+    cout << "sha512('"<< input << "'):" << output1 << endl;
+    return 0;
+}
